f08ae516ba117fdf39acaf899e3218ed6f0d492f
Sam S <66765667+SH13M@users.noreply.github.com>
2023-10-19 03:47:58 -0400

    change static methods to private helper functions

--- dpp.py
+++ dpp.py
@@ -5,64 +5,7 @@ from itertools import combinations
 class DPP():
     def __init__(self, clauses: Set[FrozenSet[Proposition]]) -> None:
         self._clauses = clauses 
-        self._props = self._get_props()
-    
-    @staticmethod
-    def _has_pnp(clause: FrozenSet[Proposition], p: Proposition) -> bool:
-        val = 0
-        for prop in clause:
-            if (prop.name == p.name and prop.t == p.t):
-                val += 1
-                break
-        for prop in clause:
-            if (prop.name == p.name and prop.t != p.t):
-                val += 1
-                break
-        return (val == 2)
-    
-    @staticmethod
-    def _rm_pnp(S: Set[FrozenSet[Proposition]], p: Proposition) -> Set[FrozenSet[Proposition]]:
-        ans = set()
-        for clause in S:
-            if not DPP._has_pnp(clause, p):
-                ans.add(clause)
-        return ans
-    
-    @staticmethod
-    def _parent_set(S: Set[FrozenSet[Proposition]], p: Proposition) -> Set[FrozenSet[Proposition]]:
-        T = set()
-        for clause in S:
-            for prop in clause:
-                if (prop.name == p.name):
-                    T.add(clause)
-                    break
-        return T
-    
-    @staticmethod
-    def _contains(clause: FrozenSet[Proposition], p: Proposition):
-        for prop in clause:
-            if (Proposition.compare(prop, p) == 2):
-                return True
-        return False
-    
-    @staticmethod
-    def _resolution(C: FrozenSet[Proposition], D: FrozenSet[Proposition], p: Proposition) -> Union[FrozenSet[Proposition], None]:
-        if (DPP._contains(C, p) and DPP._contains(D, p.negate())) or (DPP._contains(C, p.negate()) and DPP._contains(D, p)):
-            resolvent = set(C | D)
-            resolvent.remove(p)
-            resolvent.remove(p.negate())
-            return frozenset(resolvent)
-        return None
-
-    @staticmethod
-    def _resolvent_set(T: Set[FrozenSet[Proposition]], p: Proposition) -> Set[FrozenSet[Proposition]]:
-        U = set()
-        for pair in combinations(T, 2):
-            C, D = pair
-            resolvent = DPP._resolution(C, D, p)
-            if resolvent is not None:
-                U.add(resolvent)
-        return U
+        self._props = self._get_props() 
     
     def _get_props(self) -> Set[Proposition]:
         prop_names = set()
@@ -77,8 +20,61 @@ class DPP():
         T = None
         U = None
         for p in self._props:
-            S = DPP._rm_pnp(S, p)
-            T = DPP._parent_set(S, p)
-            U = DPP._resolvent_set(T, p)
+            S = _rm_pnp(S, p)
+            T = _parent_set(S, p)
+            U = _resolvent_set(T, p)
             S = (set(S) - set(T)) | set(U)
         print(S)
+
+
+def _has_pnp(clause: FrozenSet[Proposition], p: Proposition) -> bool:
+    val = 0
+    for prop in clause:
+        if (prop.name == p.name and prop.t == p.t):
+            val += 1
+            break
+    for prop in clause:
+        if (prop.name == p.name and prop.t != p.t):
+            val += 1
+            break
+    return (val == 2)
+
+def _rm_pnp(S: Set[FrozenSet[Proposition]], p: Proposition) -> Set[FrozenSet[Proposition]]:
+    ans = set()
+    for clause in S:
+         if not _has_pnp(clause, p):
+            ans.add(clause)
+    return ans
+
+def _contains(clause: FrozenSet[Proposition], p: Proposition):
+    for prop in clause:
+        if (Proposition.compare(prop, p) == 2):
+            return True
+    return False
+
+
+def _resolution(C: FrozenSet[Proposition], D: FrozenSet[Proposition], p: Proposition) -> Union[FrozenSet[Proposition], None]:
+    if (_contains(C, p) and _contains(D, p.negate())) or (_contains(C, p.negate()) and _contains(D, p)):
+        resolvent = set(C | D)
+        resolvent.remove(p)
+        resolvent.remove(p.negate())
+        return frozenset(resolvent)
+    return None
+    
+def _parent_set(S: Set[FrozenSet[Proposition]], p: Proposition) -> Set[FrozenSet[Proposition]]:
+    T = set()
+    for clause in S:
+        for prop in clause:
+            if (prop.name == p.name):
+                T.add(clause)
+                break
+    return T
+
+def _resolvent_set(T: Set[FrozenSet[Proposition]], p: Proposition) -> Set[FrozenSet[Proposition]]:
+    U = set()
+    for pair in combinations(T, 2):
+        C, D = pair
+        resolvent = _resolution(C, D, p)
+        if resolvent is not None:
+           U.add(resolvent)
+    return U
