8127bab208a1bdc100a4b5959229f477bee501b3
Sam S <66765667+SH13M@users.noreply.github.com>
2023-10-19 15:25:50 -0400

    add documentation

--- proposition.py
+++ proposition.py
@@ -1,10 +1,50 @@
 class Proposition():
+    """
+    A logical proposition
+    
+    ...
+    
+    Parameters
+    ----------
+    name : str
+        Set of clauses to perform DPP on, clauses are assumed to be disjunctive
+    t : bool
+        Negation state (True = p, False = not p)
+        
+    
+    Methods
+    -------
+    compate()
+        Compares how similar two propositions are
+    negates()
+        Gets negation of a proposition
+    
+    """
+    
     def __init__(self, name: str, t: bool) -> None:
         self.name = name
         self.t = t
         
     @staticmethod
     def compare(prop_1: 'Proposition', prop_2: 'Proposition') -> int:
+        """Compares two propositions
+        
+        Parameters
+        ----------
+        prop_1 : Proposition
+            First proposition to compare
+        prop_2 : Proposition
+            Second proposition to compare
+        
+        Returns
+        -------
+        val : int
+            The comparison value of the two propositions
+            0 means propositions have different symbols
+            1 means propositions have same symbols but different negation state
+            2 means propositions have both same symbols and negation states
+            
+        """
         val = 0
         if (prop_1.name == prop_2.name):
             val += 1
@@ -13,6 +53,7 @@ class Proposition():
         return val
 
     def negate(self) -> 'Proposition':
+        """Gets the negation of the itself"""
         return Proposition(self.name, not self.t)
 
     def __eq__(self, other: 'Proposition') -> bool:
